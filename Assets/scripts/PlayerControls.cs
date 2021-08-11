28aefbbe3f8544ff869e18085d8d69646d20d206
noamriahi <noamriahi10@gmail.com>
2021-08-11 21:36:27 +0300

    change the game

--- Assets/scripts/PlayerControls.cs
+++ Assets/scripts/PlayerControls.cs
@@ -1,15 +1,16 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-    [Header("general SetUp Settings")]
-    [Tooltip("how fast ship move up and down")][SerializeField] float controlSpeed = 15f;
+    [Header("General Setup Settings")]
+    [Tooltip("How fast ship moves up and down based upon player input")] 
+    [SerializeField] float controlSpeed = 10f;
     [Tooltip("How fast player moves horizontally")] [SerializeField] float xRange = 10f;
     [Tooltip("How fast player moves vertically")] [SerializeField] float yRange = 7f;
- 
+    
     [Header("Laser gun array")]
     [Tooltip("Add all player lasers here")]
     [SerializeField] GameObject[] lasers;
@@ -17,36 +18,36 @@ public class PlayerControls : MonoBehaviour
     [Header("Screen position based tuning")]
     [SerializeField] float positionPitchFactor = -2f;
     [SerializeField] float positionYawFactor = 2f;
-    
+
     [Header("Player input based tuning")]
-    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float controlPitchFactor = -10f;
     [SerializeField] float controlRollFactor = -20f;
 
-    float yThrow, xThrow;
-    // Update is called once per frame
+    float xThrow, yThrow;
+
     void Update()
     {
         ProcessTranslation();
         ProcessRotation();
         ProcessFiring();
     }
-    
+
     void ProcessRotation()
     {
         float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
         float pitchDueToControlThrow = yThrow * controlPitchFactor;
-
-        float pitch =  pitchDueToPosition + pitchDueToControlThrow;
+        
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
         float yaw = transform.localPosition.x * positionYawFactor;
         float roll = xThrow * controlRollFactor;
-       
-        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
+        
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
-    void ProcessTranslation(){
+    void ProcessTranslation()
+    {
         xThrow = Input.GetAxis("Horizontal");
         yThrow = Input.GetAxis("Vertical");
-
 
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
@@ -56,23 +57,27 @@ public class PlayerControls : MonoBehaviour
         float rawYPos = transform.localPosition.y + yOffset;
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
-
-            transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
     }
-    void ProcessFiring(){
-        if(Input.GetButton("Fire1")){
+
+    void ProcessFiring()
+    {
+        if (Input.GetButton("Fire1"))
+        {
             SetLasersActive(true);
         }
-        else{
+        else
+        {
             SetLasersActive(false);
         }
     }
-    void SetLasersActive(bool isActive){
+
+    void SetLasersActive(bool isActive)
+    {
         foreach (GameObject laser in lasers)
         {
             var emissionModule = laser.GetComponent<ParticleSystem>().emission;
             emissionModule.enabled = isActive;
         }
     }
-
 }
