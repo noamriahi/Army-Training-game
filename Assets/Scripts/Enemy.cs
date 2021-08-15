0af9949d4ed87211aa74dcf5b1aaca32122f3160
noamriahi <noamriahi10@gmail.com>
2021-08-15 22:52:30 +0300

    new organiation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,11 +5,47 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
-    void OnParticleCollision(GameObject other) 
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 4;
+
+    ScoreBoard scoreBoard;
+    GameObject parentGameObject;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        AddRigidbody();
+    }
+
+    void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parentGameObject.transform;
+        hitPoints--;
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
-    }
+    }    
 }
--- Assets/Scripts/SelfDestruct.cs
+++ Assets/Scripts/SelfDestruct.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class SelfDestruct : MonoBehaviour
 {
-    [SerializeField] float timeToDestroy = 3f;
+    [SerializeField] float timeTillDestroy = 3f;
+    // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject,timeToDestroy);
+       Destroy(gameObject, timeTillDestroy); 
     }
+
 }
